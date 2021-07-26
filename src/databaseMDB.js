32a4ebc5937309ddb455a20bf61e0474d9d8a8d1
Franks2695 <frankssanay@gmail.com>
2021-07-26 17:42:13 -0500

    MongoDB

--- src/databaseMDB.js
+++ src/databaseMDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/PI_58', {
+mongoose.connect('mongodb+srv://Sorel:violadores@viernes.2qyqm.mongodb.net/PI_58?retryWrites=true&w=majority', {
         useCreateIndex: true,
         useFindAndModify: false,
         useNewUrlParser: true,
@@ -8,3 +8,13 @@ mongoose.connect('mongodb://localhost:27017/PI_58', {
     })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
+
+/* let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/PI_58'
+} else {
+    urlDB = 'mongodb+srv://Sorel:violadores@viernes.2qyqm.mongodb.net/PI_58?retryWrites=true&w=majority'
+}
+
+process.env.URLDB = urlDB; */
